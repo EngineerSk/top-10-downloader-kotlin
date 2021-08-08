45e9f9df0c2e0baac9172ff8c097ab5efb0fd17a
oriade-soft-dev <durosinmi.oriade@babbangona.com>
2021-08-08 11:23:07 +0100

    xml records parsed into Arraylist of FeedEntry and populated into list view using ArrayAdapter

--- app/src/main/java/com/engineersk/top10downloader/MainActivity.kt
+++ app/src/main/java/com/engineersk/top10downloader/MainActivity.kt
@@ -1,28 +1,65 @@
 package com.engineersk.top10downloader
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import kotlin.properties.Delegates
+
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            release = $releaseDate
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+
+}
 
 class MainActivity : AppCompatActivity() {
+
     private val TAG = "MainActivity"
+    private lateinit var xmlListView: ListView
+    private val downloadData by lazy {
+        DownloadData(this, xmlListView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: called...")
-        val downloadData = DownloadData()
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        xmlListView = findViewById(R.id.xmlListView)
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=40/xml")
         Log.d(TAG, "onCreate: Done!!!")
     }
 
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>() {
+        private class DownloadData(context: Context, listView: ListView) :
+            AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
+
             override fun doInBackground(vararg urls: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${urls[0]}")
                 val rssFeed: String = downloadXML(urls[0])
@@ -34,7 +71,13 @@ class MainActivity : AppCompatActivity() {
 
             override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                Log.d(TAG, "onPostExecute: parameter is $result")
+                val parseApplications = ParseApplications()
+                parseApplications.parse(result)
+                val arrayAdapter = ArrayAdapter(
+                    propContext, R.layout.list_item,
+                    parseApplications.applications
+                )
+                propListView.adapter = arrayAdapter
             }
 
             private fun downloadXML(urlPath: String?): String {
@@ -42,7 +85,8 @@ class MainActivity : AppCompatActivity() {
 
                 try {
                     val url = URL(urlPath)
-                    val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    val urlConnection: HttpURLConnection =
+                        url.openConnection() as HttpURLConnection
                     val response = urlConnection.responseCode
                     Log.d(TAG, "downloadXML: The response code was $response")
 
@@ -71,4 +115,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        downloadData.cancel(true)
+    }
+
 }
